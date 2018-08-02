68b9b8e33ee601f5c740fa951e01d9ed5137af78
juntaekoh <35521568+juntaekoh@users.noreply.github.com>
2018-08-03 04:17:37 +0900

    2

--- HelloDisabilityProject/app.py
+++ HelloDisabilityProject/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/a/<path>')
+def goPath(path=None):
+    if path:
+       return render_template("%s.html" % path)
+    else : return 'a'
+
+
+
 
 
 if __name__ == '__main__':

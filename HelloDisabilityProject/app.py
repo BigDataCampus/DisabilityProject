ca92ca9f9463ad31e14119648cfa14a0475cc37a
juntaekoh <35521568+juntaekoh@users.noreply.github.com>
2018-08-10 17:56:26 +0900

    180810(Fri)-2

--- HelloDisabilityProject/app.py
+++ HelloDisabilityProject/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
+import json
 
 
 app = Flask(__name__)
@@ -29,6 +30,18 @@ class Place(db.Model):
     lat = db.Column(db.FLOAT, nullable=False)
     lng = db.Column(db.FLOAT, nullable=False)
     category = db.Column(db.String(20), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'placeID':self.placeID,
+            'place_name':self.place_name,
+            'place_address' : self.place_address,
+            'lat' : self.lat,
+            'lng' : self.lng,
+            'category' : self.category
+        }
 
 class Facility(db.Model):
     facilityID = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -57,7 +70,13 @@ def showPlace():
     print(places)
     return render_template("show.html", places = places)
 
-
+@app.route('/listing')
+def PlaceSetting():
+    places = Place.query.all()
+    print("pp",places)
+    json_list = [i.serialize for i in places]
+    print(json_list)
+    return render_template('base2.html', places = json_list)
 
 if __name__ == '__main__':
     app.run()
